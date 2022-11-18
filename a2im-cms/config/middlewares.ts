0638b15e91fb714592a49aeee7083b158d4e1a3c
wobble-house <ross@wobble.house>
2022-11-18 12:03:27 -0500

    updating middlewares for image hosting

--- a2im-cms/config/middlewares.ts
+++ a2im-cms/config/middlewares.ts
@@ -1,6 +1,16 @@
-export default [
+export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET')}.s3.amazonaws.com`],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
