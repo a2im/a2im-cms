9c57b13958f44adab87f9969b5115b703386c2da
wobble-house <ross@wobble.house>
2023-01-18 17:38:24 -0500

    updated url

--- a2im-cms/config/server.ts
+++ a2im-cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT'),
-
+  url: 'https://cms.a2im.org',
   app: {
     keys: ['APP_KEY1', 'APP_KEY2', 'APP_KEY3', 'APP_KEY4'],
   },
