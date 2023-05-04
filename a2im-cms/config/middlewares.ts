2acadf542b4f04240bdf27b1edcd167f05278d2f
wobble.house <ross@wobble.house>
2023-05-04 19:03:52 -0400

    updating strapi & adding title sponsor

--- a2im-cms/config/middlewares.ts
+++ a2im-cms/config/middlewares.ts
@@ -1,26 +1,6 @@
 export default ({ env }) => [
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'cms.a2im.org',
-            'strapi.io',
-            `${env('AWS_BUCKET_NAME')}.s3.amazonaws.com`
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
--- a2im-cms/config/plugins.ts
+++ a2im-cms/config/plugins.ts
@@ -3,12 +3,19 @@ export default ({ env }) => ({
         config: {
             provider: 'aws-s3',
             providerOptions: {
+              s3Options: {
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
                 secretAccessKey: env('AWS_ACCESS_SECRET'),
                 region: env('AWS_REGION'),
                 params: {
-                    Bucket: env('AWS_BUCKET_NAME'),
+                  Bucket: env('AWS_BUCKET_NAME'),
                 },
+              },
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
             },
         },
     },
