2645967b58195298321a0e33aac17bb7afc9ade3
wobble.house <ross@wobble.house>
2023-05-04 19:54:46 -0400

    updating img src

--- a2im-cms/config/middlewares.ts
+++ a2im-cms/config/middlewares.ts
@@ -1,6 +1,26 @@
 export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          'script-src': ["'self'", "'unsafe-inline'"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "cms.a2im.org",
+            "strapi.io",
+            `${env('AWS_BUCKET_NAME')}.s3.amazonaws.com`
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
